d799cfe08fc4294537bef76f0165972a278aa09d
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2014-01-13 19:50:20 +0100

    Add link analyser python code.

---
+++ link_analysis/link.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014, Niklas Hauser
+# All rights reserved.
+# 
+# The file is part of my bachelor thesis and is released under the 3-clause BSD
+# license. See the file `LICENSE` for the full license governing this code.
+# -----------------------------------------------------------------------------
+
+from logger import Logger
+
+class Link:
+	""" Link
+	Represents a link in a non-platform specific format.
+	This encapsulates payload, errors, qualitiy quantifiers like signal strength,
+	and power settings.
+	"""
+
+	def __init__(self, properties=None, logger=None):
+		if logger == None:
+			self.log = Logger()
+		else:
+			self.log = logger
+		
+		self.properties = {}
+		
+		if properties:
+			self.properties = properties
+	
+	def __iter__(self):
+		for key in self.properties:
+			yield key
+	
+	def __setitem__(self, key, value):
+		self.properties[key] = value
+	
+	def __getitem__(self, key):
+		return self.properties[key]
+	
+	def __len__(self):
+		return len(self.properties)
+	
+	def __hash__(self):
+		return str(self)
+	
+	def __repr__(self):
+		return self.__str__()
+
+	def __str__(self):
+		return ("Link(\t%s )\n" %
+			",\n".join(["'%s': '%s'" % (p, self.properties[p]) for p in self.properties])) \
+			.replace("\n", "\n\t")
---
+++ link_analysis/string_link.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014, Niklas Hauser
+# All rights reserved.
+# 
+# The file is part of my bachelor thesis and is released under the 3-clause BSD
+# license. See the file `LICENSE` for the full license governing this code.
+# -----------------------------------------------------------------------------
+
+
+from logger import Logger
+from link import Link
+import dateutil.parser
+
+class StringLink(Link):
+	""" StringLink
+	Parses a string and generates a link class 
+	"""
+
+	def __init__(self, string=None, logger=None):
+		Link.__init__(self, None, logger)
+		
+		if string:
+			self['string'] = string
+			for item in string.split('\t'):
+				items = item.split('=')
+				self.properties[items[0]] = items[1]
+		
+		for p in self.properties:
+			if p in ['power', 'lqi', 'crc', 'rssi', 'errors']:
+				self[p] = int(self[p])
+			elif p in ['timestamp']:
+				self[p] = dateutil.parser.parse(self[p])
+			elif p in ['orgdata', 'xor']:
+				self[p] = bytearray( [int(ii) for ii in self[p].split(' ')] )
+
+	def __str__(self):
+		return "String" + Link.__str__(self)
