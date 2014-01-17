bedfac69e49d1b35229ee6cbca308acfa4a57dec
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2014-01-17 12:25:58 +0100

    Add mig generated python messages.

---
+++ experiment_control/messages/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014, Niklas Hauser
+# All rights reserved.
+# 
+# The file is part of my bachelor thesis and is released under the 3-clause BSD
+# license. See the file `LICENSE` for the full license governing this code.
+# -----------------------------------------------------------------------------
+
+__all__ = ["RadioMessage", "SensorMessage", "SerialMessage"]
