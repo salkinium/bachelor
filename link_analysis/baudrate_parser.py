f0f7582a1b96e59eabf3806ca8ea9611677a1789
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2014-05-28 18:46:09 +0200

    Update baudrate_parser to make it more beautiful.

--- link_analysis/baudrate_parser.py
+++ link_analysis/baudrate_parser.py
@@ -16,6 +16,10 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
+import pandas as pd
+import seaborn as sns
+sns.set_style("whitegrid")
+
 from matplotlib import rc
 import csv
 
@@ -47,7 +51,7 @@ def get_values(file, start_row, rows, temperature_column, value_column):
 
 def plot_values(ax, values, color='k'):
     plot = ax.plot(values['temperature'], values['values'], '{}-'.format(color),
-                   values['temperature'], values['values'], '{}o'.format(color))
+                   values['temperature'], values['values'], '{}o'.format(color), markersize=5)
     pylab.setp(plot[0], linewidth=2)
 
     return plot
@@ -57,7 +61,7 @@ def plot_table_values(ax, values, color='k', pol=1):
     fit = pylab.polyfit(values['temperature'], values['values'], pol)
     fit_fn = pylab.poly1d(fit)
     plot = ax.plot(values['temperature'], fit_fn(values['temperature']), '{}-'.format(color),
-                   values['temperature'], values['values'], '{}o'.format(color))
+                   values['temperature'], values['values'], '{}o'.format(color), markersize=5)
     pylab.setp(plot[0], linewidth=2)
 
     return plot
@@ -76,7 +80,7 @@ def plot_dco_values(ax, values, color='k'):
     plot = ax.plot(interpol1['temperature'], fit_fn1(interpol1['temperature']), 'k-',
                    interpol2['temperature'], fit_fn2(interpol2['temperature']), 'k-',
                     # values['temperature'], values['values'], '{}-'.format(color),
-                   values['temperature'], values['values'], '{}o'.format(color))
+                   values['temperature'], values['values'], '{}o'.format(color), markersize=5)
     pylab.setp(plot[0], linewidth=2)
     pylab.setp(plot[1], linewidth=2)
 
@@ -87,7 +91,7 @@ def plot_linear_regression_values(ax, values, color='k'):
     print 'm={} b={}'.format(fit[0], fit[1])
     fit_fn = pylab.poly1d(fit)
     plot = ax.plot(values['temperature'], fit_fn(values['temperature']), '{}-'.format(color),
-                   values['temperature'], values['values'], '{}o'.format(color))
+                   values['temperature'], values['values'], '{}o'.format(color), markersize=5)
     pylab.setp(plot[0], linewidth=2)
 
     return plot
@@ -125,6 +129,7 @@ if __name__ == "__main__":
 
     # plot four baudrate errors
     fig, ax = plt.subplots(1)
+    fig.set_size_inches(8 * 0.625, 0.625 * 1 * 5.5)
     ax.set_xlim(xmin=15, xmax=95)
 
     b115200_line, b115200_dot = plot_linear_regression_values(ax, baudrate_115200_error, 'k')
@@ -132,69 +137,72 @@ if __name__ == "__main__":
     b19200_line, b19200_dot = plot_linear_regression_values(ax, baudrate_19200_error, 'r')
     b4800_line, b4800_dot = plot_linear_regression_values(ax, baudrate_4800_error, 'g')
 
-    ax.set_ylabel("Relative Error (%)", fontsize=16)
-    ax.set_xlabel("Temperature ($^\circ$C)", fontsize=16)
-    ax.grid(b=True, which='major', color='0.66', linestyle='-')
-    ax.axhline(5, linestyle='--', color='0.33', linewidth=2)
-    ax.axhline(-5, linestyle='--', color='0.33', linewidth=2)
+    ax.set_ylabel("Relative Error (%)", fontsize=18)
+    ax.set_xlabel("Temperature ($^\circ$C)", fontsize=18)
+    ax.grid(b=True, which='major', linestyle='-')
+    ax.axhline(5, linestyle='--', color='0.66', linewidth=2, zorder=1)
+    ax.axhline(-5, linestyle='--', color='0.66', linewidth=2, zorder=1)
     ax.set_axisbelow(True)
 
-    pylab.legend([b4800_line, b19200_line, b115200_line, b57600_line],
+    l = pylab.legend([b4800_line, b19200_line, b115200_line, b57600_line],
                  ["4800 Baud", "19200 Baud", "115200 Baud", "57600 Baud"],
-                 prop = {'size': 16})
+                 prop = {'size': 11})
+    l.set_zorder(10)
 
-    pylab.savefig(os.path.join(os.path.dirname(__file__), "{}_baudrate_error.pdf".format(baudrate_file)))
+    pylab.savefig(os.path.join(os.path.dirname(__file__), "{}_baudrate_error.pdf".format(baudrate_file)), bbox_inches='tight', pad_inches=0.1)
     print (-0.364828571429 - 0.36455 - 0.361257142857 - 0.378014285714) / 4.0
 
     # plot dco error
     fig2, ax2 = plt.subplots(1)
+    fig2.set_size_inches(8 * 0.625, 0.625 * 1 * 5.5)
     ax2.set_ylim(ymin=-25, ymax=15)
     ax2.set_xlim(xmin=20, xmax=95)
     dco_error_line= plot_dco_values(ax2, reboot_dco_error, 'k')
 
-    ax2.set_ylabel("Relative Error (%)", fontsize=16)
-    ax2.set_xlabel("Temperature ($^\circ$C)", fontsize=16)
-    ax2.grid(b=True, which='major', color='0.66', linestyle='-')
-    ax2.axhline(5, linestyle='--', color='0.33', linewidth=2)
-    ax2.axhline(-5, linestyle='--', color='0.33', linewidth=2)
+    ax2.set_ylabel("Relative Error (%)", fontsize=18)
+    ax2.set_xlabel("Temperature ($^\circ$C)", fontsize=18)
+    ax2.grid(b=True, which='major', linestyle='-')
+    ax2.axhline(5, linestyle='--', color='0.66', linewidth=2, zorder=1)
+    ax2.axhline(-5, linestyle='--', color='0.66', linewidth=2, zorder=1)
     ax2.set_axisbelow(True)
 
-    pylab.savefig(os.path.join(os.path.dirname(__file__), "{}_dco_error.pdf".format(reboot_file)))
+    pylab.savefig(os.path.join(os.path.dirname(__file__), "{}_dco_error.pdf".format(reboot_file)), bbox_inches='tight', pad_inches=0.1)
 
     # plot inverse table
     fig3, ax3 = plt.subplots(1)
+    fig3.set_size_inches(8 * 0.625, 0.625 * 1 * 5.5)
     # ax3.set_ylim(ymin=-25, ymax=15)
     ax3.set_xlim(xmin=-10, xmax=125)
     # baudrate_table_line = plot_table_values(ax3, baudrate_115200_expected_inverse, 'b', 2)
     baudrate_table_line = plot_table_values(ax3, baudrate_115200_real_inverse, 'k', 1)
 
-    ax3.set_ylabel("Baudrate (kbps)", fontsize=16)
-    ax3.set_xlabel("Temperature ($^\circ$C)", fontsize=16)
-    ax3.grid(b=True, which='major', color='0.66', linestyle='-')
-    ax3.axhline(115.200, linestyle='-', color='0.33', linewidth=1.5, zorder=1)
+    ax3.set_ylabel("Baudrate (kBaud)", fontsize=18)
+    ax3.set_xlabel("Temperature ($^\circ$C)", fontsize=18)
+    ax3.grid(b=True, which='major', linestyle='-')
+    ax3.axhline(115.200, linestyle='-', color='0.66', linewidth=1.5, zorder=1)
     ax3.set_axisbelow(True)
 
-    pylab.savefig(os.path.join(os.path.dirname(__file__), "{}_table.pdf".format(baudrate_file)))
+    pylab.savefig(os.path.join(os.path.dirname(__file__), "{}_table.pdf".format(baudrate_file)), bbox_inches='tight', pad_inches=0.1)
 
     # plot correction error
     fig4, ax4 = plt.subplots(1)
+    fig4.set_size_inches(8 * 0.625, 0.625 * 1 * 5.5)
     ax4.set_ylim(ymin=-25, ymax=15)
     ax4.set_xlim(xmin=20, xmax=95)
     correction_high = plot_values(ax4, correction_115200_high_start_error, 'b')
     correction_low = plot_values(ax4, correction_115200_low_start_error, 'k')
 
-    ax4.set_ylabel("Relative Error (%)", fontsize=16)
-    ax4.set_xlabel("Temperature ($^\circ$C)", fontsize=16)
-    ax4.grid(b=True, which='major', color='0.66', linestyle='-')
-    ax4.axhline(5, linestyle='--', color='0.33', linewidth=2)
-    ax4.axhline(0, linestyle='-', color='0.33', linewidth=1, zorder=1)
-    ax4.axhline(-5, linestyle='--', color='0.33', linewidth=2)
+    ax4.set_ylabel("Relative Error (%)", fontsize=18)
+    ax4.set_xlabel("Temperature ($^\circ$C)", fontsize=18)
+    ax4.grid(b=True, which='major', linestyle='-')
+    ax4.axhline(5, linestyle='--', color='0.66', linewidth=2, zorder=1)
+    ax4.axhline(-5, linestyle='--', color='0.66', linewidth=2, zorder=1)
     ax4.set_axisbelow(True)
 
     pylab.legend([correction_low[0], correction_high[0]],
                  ["Reboot at $24^{\circ}$C", "Reboot at $88^{\circ}$C"],
-                 prop={'size': 16})
+                 prop={'size': 13})
 
-    pylab.savefig(os.path.join(os.path.dirname(__file__), "{}_correction_error.pdf".format(correction_file)))
+    pylab.savefig(os.path.join(os.path.dirname(__file__), "{}_correction_error.pdf".format(correction_file)), bbox_inches='tight', pad_inches=0.1)
 
 
